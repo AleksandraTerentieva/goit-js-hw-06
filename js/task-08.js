749e8ceaaae7b02ba943168233b1fbcd48b3ae71
AleksandraTerentieva <terenteva.9529@gmail.com>
2022-10-26 00:26:43 +0300

    finished task 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSumbitForm);
+
+function onSumbitForm(event) {
+  event.preventDefault();
+
+  if (
+    event.currentTarget.elements.email.value === "" ||
+    event.currentTarget.elements.password.value === ""
+  ) {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  const formObj = {
+    email,
+    password,
+  };
+  console.log(formObj);
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const span = document.querySelector("span");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+}
